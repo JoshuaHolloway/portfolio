016693abdf5edeb783dd738aad52968bb027f9b0
JoshuaHolloway <jhollow6@asu.edu>
2022-11-06 14:38:32 -0600

    page-3d web component with self closing tag

--- src/web-comps/pages/3d.js
+++ src/web-comps/pages/3d.js
@@ -32,144 +32,144 @@ class WebComp extends HTMLElement {
   }
 
   connectedCallback() {
-    const elem = this.shadowRoot.querySelector('#web-comp');
+    // const elem = this.shadowRoot.querySelector('#web-comp');
 
-      const raycaster = new THREE.Raycaster();
-      const scene = new THREE.Scene();
-      const camera = new THREE.PerspectiveCamera(
-        75,                       // field of view [deg.]
-        innerWidth / innerHeight, // aspect ratio of scene
-        0.1,                      // clipping plane (near)
-        1000,                     // clipping plane (far)
-      );
+    const raycaster = new THREE.Raycaster();
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera(
+      75,                       // field of view [deg.]
+      innerWidth / innerHeight, // aspect ratio of scene
+      0.1,                      // clipping plane (near)
+      1000,                     // clipping plane (far)
+    );
 
-      const renderer = new THREE.WebGLRenderer();
+    const renderer = new THREE.WebGLRenderer();
 
-      renderer.setSize(window.innerWidth, window.innerHeight);
-      // renderer.setPixelRatio(window.devicePixelRation);
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    // renderer.setPixelRatio(window.devicePixelRation);
 
-      this.shadowRoot.querySelector('#threeD-page').appendChild(renderer.domElement);
+    this.shadowRoot.querySelector('#threeD-page').appendChild(renderer.domElement);
 
-      // Specify what camera to control and where to render in DOM:
-      new OrbitControls(camera, renderer.domElement);
+    // Specify what camera to control and where to render in DOM:
+    new OrbitControls(camera, renderer.domElement);
 
-      // - - - - - - - - - - - - - - - - - - - - 
+    // - - - - - - - - - - - - - - - - - - - - 
 
-      const boxGeometry = new THREE.BoxGeometry(1, 1, 1);
-      const material1 = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-      const cube = new THREE.Mesh(boxGeometry, material1);
-      scene.add(cube);
+    const boxGeometry = new THREE.BoxGeometry(1, 1, 1);
+    const material1 = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+    const cube = new THREE.Mesh(boxGeometry, material1);
+    scene.add(cube);
 
-      // - - - - - - - - - - - - - - - - - - - - 
+    // - - - - - - - - - - - - - - - - - - - - 
 
-      const planeGeometry = new THREE.PlaneGeometry(5, 5, 10, 10);
-      const material2 = new THREE.MeshPhongMaterial({ 
-        color: 0x000fff,
-        side: THREE.DoubleSide,
-        flatShading: THREE.FlatShading
-      });
-      const plane = new THREE.Mesh(planeGeometry, material2);
-      scene.add(plane);
+    const planeGeometry = new THREE.PlaneGeometry(5, 5, 10, 10);
+    const material2 = new THREE.MeshPhongMaterial({ 
+      color: 0x000fff,
+      side: THREE.DoubleSide,
+      flatShading: THREE.FlatShading
+    });
+    const plane = new THREE.Mesh(planeGeometry, material2);
+    scene.add(plane);
 
-      console.log('plane mesh vertices: ', 
-        plane.geometry.attributes.position.array // position contains all the data related to each vertex [x1, y1, z1, x2, y2, z2, ...]
-      );
+    console.log('plane mesh vertices: ', 
+      plane.geometry.attributes.position.array // position contains all the data related to each vertex [x1, y1, z1, x2, y2, z2, ...]
+    );
 
-      const { array } = plane.geometry.attributes.position;
-      for (let i = 0; i < array.length; i += 3) {
-        const x = array[i];
-        const y = array[i+1];
-        const z = array[i+2];
+    const { array } = plane.geometry.attributes.position;
+    for (let i = 0; i < array.length; i += 3) {
+      const x = array[i];
+      const y = array[i+1];
+      const z = array[i+2];
 
-        array[i] = x + 3;
-        array[i + 2] = z + Math.random();
+      array[i] = x + 3;
+      array[i + 2] = z + Math.random();
+    }
+
+    // const generatePlane = () => {
+    //   plane.geometry.dispose();
+    //   plane.geometry = new THREE.PlaneGeometry(world.plane.width, world.plane.height, world.plane.width_segments, world.plane.height_segments);
+    //   console.log('world.plane.width', world.plane.width);
+
+    //   // Reset vertices
+    //   const { array } = plane.geometry.attributes.position;
+    //   for (let i = 0; i < array.length; i += 3) {
+    //     const x = array[i];
+    //     const y = array[i+1];
+    //     const z = array[i+2];
+
+    //     array[i] = x + 3;
+    //     array[i + 2] = z + Math.random();
+    //   }
+    // };
+
+    // const gui = new dat.GUI();
+    // const world = {
+    //   plane: {
+    //     width: 5,
+    //     height: 5,
+    //     width_segments: 10,
+    //     height_segments: 10
+    //   },
+    // };
+    // console.log('gui: ', gui);
+    // gui.add(world.plane, "width", 1, 10,).onChange(generatePlane);
+    // gui.add(world.plane, "height", 1, 10,).onChange(generatePlane);
+    // gui.add(world.plane, "width_segments", 1, 10,).onChange(generatePlane);
+    // gui.add(world.plane, "height_segments", 1, 10,).onChange(generatePlane);
+
+    // - - - - - - - - - - - - - - - - - - - - 
+    
+    const light = new THREE.DirectionalLight(0xffffff, 1, );
+    light.position.set(0, 0, 1); // x, y, z
+    scene.add(light);
+
+    const back_light = new THREE.DirectionalLight(0xffffff, 1, );
+    back_light.position.set(0, 0, -1); // x, y, z
+    scene.add(back_light);
+
+    // - - - - - - - - - - - - - - - - - - - - 
+    // Events
+
+    const mouse = {
+      x: undefined,
+      y: undefined,
+      set(x, y) {
+        this.x = +(x / innerWidth)  * 2 - 1;
+        this.y = -(y / innerHeight) * 2 + 1;
+      }
+    };
+
+    window.addEventListener('mousemove', (event) => {
+      mouse.set(event.clientX, event.clientY);
+
+      raycaster.setFromCamera(mouse, camera);
+      const intersects = raycaster.intersectObject(plane);
+      if (intersects.length > 0) {
+        console.log('intersecting');
       }
 
-      // const generatePlane = () => {
-      //   plane.geometry.dispose();
-      //   plane.geometry = new THREE.PlaneGeometry(world.plane.width, world.plane.height, world.plane.width_segments, world.plane.height_segments);
-      //   console.log('world.plane.width', world.plane.width);
+      // console.log('mouse: ', mouse);
+      // console.log('intersects: ', intersects);
+    });
 
-      //   // Reset vertices
-      //   const { array } = plane.geometry.attributes.position;
-      //   for (let i = 0; i < array.length; i += 3) {
-      //     const x = array[i];
-      //     const y = array[i+1];
-      //     const z = array[i+2];
+    // - - - - - - - - - - - - - - - - - - - - 
 
-      //     array[i] = x + 3;
-      //     array[i + 2] = z + Math.random();
-      //   }
-      // };
+    camera.position.z = 5;
 
-      // const gui = new dat.GUI();
-      // const world = {
-      //   plane: {
-      //     width: 5,
-      //     height: 5,
-      //     width_segments: 10,
-      //     height_segments: 10
-      //   },
-      // };
-      // console.log('gui: ', gui);
-      // gui.add(world.plane, "width", 1, 10,).onChange(generatePlane);
-      // gui.add(world.plane, "height", 1, 10,).onChange(generatePlane);
-      // gui.add(world.plane, "width_segments", 1, 10,).onChange(generatePlane);
-      // gui.add(world.plane, "height_segments", 1, 10,).onChange(generatePlane);
+    function animate() {
+      requestAnimationFrame(animate);
+      renderer.render(scene, camera);
+      cube.rotation.x += 0.01;
+      cube.rotation.y += 0.01;
+    }
+    animate();
 
-      // - - - - - - - - - - - - - - - - - - - - 
-      
-      const light = new THREE.DirectionalLight(0xffffff, 1, );
-      light.position.set(0, 0, 1); // x, y, z
-      scene.add(light);
-
-      const back_light = new THREE.DirectionalLight(0xffffff, 1, );
-      back_light.position.set(0, 0, -1); // x, y, z
-      scene.add(back_light);
-
-      // - - - - - - - - - - - - - - - - - - - - 
-      // Events
-
-      const mouse = {
-        x: undefined,
-        y: undefined,
-        set(x, y) {
-          this.x = +(x / innerWidth)  * 2 - 1;
-          this.y = -(y / innerHeight) * 2 + 1;
-        }
-      };
-
-      window.addEventListener('mousemove', (event) => {
-        mouse.set(event.clientX, event.clientY);
-
-        raycaster.setFromCamera(mouse, camera);
-        const intersects = raycaster.intersectObject(plane);
-        if (intersects.length > 0) {
-          console.log('intersecting');
-        }
-
-        // console.log('mouse: ', mouse);
-        // console.log('intersects: ', intersects);
-      });
-
-      // - - - - - - - - - - - - - - - - - - - - 
-
-      camera.position.z = 5;
-
-      function animate() {
-        requestAnimationFrame(animate);
-        renderer.render(scene, camera);
-        cube.rotation.x += 0.01;
-        cube.rotation.y += 0.01;
-      }
-      animate();
-
-      // - - - - - - - - - - - - - - - - - - - -     
+    // - - - - - - - - - - - - - - - - - - - -     
   }
 
   disconnectedCallback() {
-    const elem = this.shadowRoot.querySelector('#web-comp')
+    // const elem = this.shadowRoot.querySelector('#web-comp')
   }
 }
 
-window.customElements.define('web-comp', WebComp);
+window.customElements.define('page-3d', WebComp);
