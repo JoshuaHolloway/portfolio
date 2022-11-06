cdaa13577c90beeb47cde87d72ea825d93f8c3cb
JoshuaHolloway <jhollow6@asu.edu>
2022-11-06 12:14:14 -0600

    setupPageTransition()

--- src/index.js
+++ src/index.js
@@ -13,10 +13,6 @@ const overlay = qs('#overlay');
 const blur_container = qs('#blur-container');
 const navbar = qs('header#navbar');
 
-const setupPageTransition = () => {
-
-};
-
 const setupSideDrawer = () => {
   let is_open = false;
   const setIsOpen = (bool) => {
@@ -87,156 +83,158 @@ setupSideDrawer();
 
 // ==============================================
 
+const setupPageTransition = () => {
 
+  const pages = document.querySelectorAll('.page');
 
+  const routes = [
+    {
+      page: 'home',
+      path: '/'
+    },
+    {
+      page: 'about',
+      path: '/about'
+    },
+    {
+      page: 'portfolio',
+      path: '/portfolio'
+    },
+    {
+      page: 'contact',
+      path: '/contact'
+    },
+    {
+      page: 'threeD',
+      path: '/3D'
+    },
+  ];
 
-const pages = document.querySelectorAll('.page');
+  // Navbar navlinks (navdrawer navlinks not handled yet)
+  const navlinks = document.querySelectorAll('#navbar .navlinks > .navlink');
+  let page_idx = { current: 0, prev: null};
+  navlinks.forEach((navlink, idx) => {
 
-const routes = [
-  {
-    page: 'home',
-    path: '/'
-  },
-  {
-    page: 'about',
-    path: '/about'
-  },
-  {
-    page: 'portfolio',
-    path: '/portfolio'
-  },
-  {
-    page: 'contact',
-    path: '/contact'
-  },
-  {
-    page: 'threeD',
-    path: '/3D'
-  },
-];
+    const navlink_num = idx; 
 
-// Navbar navlinks (navdrawer navlinks not handled yet)
-const navlinks = document.querySelectorAll('#navbar .navlinks > .navlink');
-let page_idx = { current: 0, prev: null};
-navlinks.forEach((navlink, idx) => {
+    navlink.addEventListener('click', () => {
 
-  const navlink_num = idx; 
+      // update state
+      page_idx = {current: navlink_num, prev: page_idx.current};
 
-  navlink.addEventListener('click', () => {
+      // reset z-index and fade out prev-page:
+      const prev_page = pages[page_idx.prev];
+      const curr_page = pages[navlink_num];
+      gsap.to(prev_page, {
+        opacity: 0,
+        onComplete: () => {
+          prev_page.style.zIndex = 0;
+        }
+      });
 
-    // update state
-    page_idx = {current: navlink_num, prev: page_idx.current};
+      // Remove .active from prev. nav-link
+      // navlinks[page_idx.prev].classList.remove('active')
+      navlinks.forEach(x => x.classList.remove('active')); // Reset-all for simple navlink start on page refresh.
 
-    // reset z-index and fade out prev-page:
-    const prev_page = pages[page_idx.prev];
-    const curr_page = pages[navlink_num];
-    gsap.to(prev_page, {
-      opacity: 0,
-      onComplete: () => {
-        prev_page.style.zIndex = 0;
-      }
+      // Set currently clicked navlink to active
+      navlink.classList.add('active');
+
+      // Change the SPA 'page' and set the URL's path.
+      const path = routes[navlink_num].path;
+      window.history.pushState({}, '', path);
+      window.localStorage.setItem('path', JSON.stringify(path));
+      curr_page.style.zIndex = 1;
+      gsap.to(curr_page, { 
+        opacity: 1,
+      });
+
+      // Fire event for local-storage 'path' changed
+      fireEvent('local-storage-path-modified');
+      
     });
-
-    // Remove .active from prev. nav-link
-    // navlinks[page_idx.prev].classList.remove('active')
-    navlinks.forEach(x => x.classList.remove('active')); // Reset-all for simple navlink start on page refresh.
-
-    // Set currently clicked navlink to active
-    navlink.classList.add('active');
-
-    // Change the SPA 'page' and set the URL's path.
-    const path = routes[navlink_num].path;
-    window.history.pushState({}, '', path);
-    window.localStorage.setItem('path', JSON.stringify(path));
-    curr_page.style.zIndex = 1;
-    gsap.to(curr_page, { 
-      opacity: 1,
-    });
-
-    // Fire event for local-storage 'path' changed
-    fireEvent('local-storage-path-modified');
-    
   });
-});
 
-// ==============================================
+  // --------------------------------------------
 
-window.addEventListener('load', (event) => {
+  window.addEventListener('load', (event) => {
 
-  // For initial page load on /
-  startSmoothScroll();
+    // For initial page load on /
+    startSmoothScroll();
 
-  const path_local_storage = JSON.parse(window.localStorage.getItem('path'));
-  
-  // page_idx Hack: Just hard code the previous path. Can store page_idx object in local storage in want to store actual previous path.
-  
-  if (path_local_storage === null) {
-    // Initial page load => Set 'home' as active navlink
-    navlinks[0].classList.add('active');
-
-    // Set local storage 'path' to '/'
-    window.localStorage.setItem('path', JSON.stringify('/'));
-  
-    gsap.fromTo(document.querySelector('body'), {opacity: 0}, {opacity: 1, duration: 0})
-  }
-  else{
-    pages.forEach(page => {
-      page.style.zIndex = 0;
-      page.style.opacity = 0;
-    });
-
-    if (path_local_storage === '/') {
-      pages[0].style.zIndex = 1;
-      pages[0].style.opacity = 1;
+    const path_local_storage = JSON.parse(window.localStorage.getItem('path'));
+    
+    // page_idx Hack: Just hard code the previous path. Can store page_idx object in local storage in want to store actual previous path.
+    
+    if (path_local_storage === null) {
+      // Initial page load => Set 'home' as active navlink
       navlinks[0].classList.add('active');
-      page_idx = { current: 0, prev: 1 };
-    }
-    else if (path_local_storage === '/about') {
-      pages[1].style.zIndex = 1;
-      pages[1].style.opacity = 1;
-      navlinks[1].classList.add('active');
-      window.history.replaceState({}, '', '/about');
-      page_idx = { current: 1, prev: 0 };
-    }
-    else if (path_local_storage === '/portfolio') {
-      pages[2].style.zIndex = 1;
-      pages[2].style.opacity = 1;
-      navlinks[2].classList.add('active');
-      window.history.replaceState({}, '', '/portfolio');
-      page_idx = { current: 2, prev: 0 };
-    }
-    else if (path_local_storage === '/contact') {
-      pages[3].style.zIndex = 1;
-      pages[3].style.opacity = 1;
-      navlinks[3].classList.add('active');
-      window.history.replaceState({}, '', '/contact');
-      page_idx = { current: 3, prev: 0 };
-    }
-    else if (path_local_storage === '/3D') {
-      pages[4].style.zIndex = 1;
-      pages[4].style.opacity = 1;
-      navlinks[4].classList.add('active');
-      window.history.replaceState({}, '', '/3D');
-      page_idx = { current: 4, prev: 0 };
-    }
 
-    gsap.fromTo(document.querySelector('body'), {opacity: 0}, {opacity: 1, duration: 1, delay: 0.5})
+      // Set local storage 'path' to '/'
+      window.localStorage.setItem('path', JSON.stringify('/'));
+    
+      gsap.fromTo(document.querySelector('body'), {opacity: 0}, {opacity: 1, duration: 0})
+    }
+    else{
+      pages.forEach(page => {
+        page.style.zIndex = 0;
+        page.style.opacity = 0;
+      });
 
-
-    listenForEvent('local-storage-path-modified', () => {
-      console.log('Event Fired: path changed and local storage "path" set');
-
-      const path_local_storage = JSON.parse(window.localStorage.getItem('path'));
       if (path_local_storage === '/') {
-        startSmoothScroll();
+        pages[0].style.zIndex = 1;
+        pages[0].style.opacity = 1;
+        navlinks[0].classList.add('active');
+        page_idx = { current: 0, prev: 1 };
       }
-      else {
-        stopSmoothScroll();
+      else if (path_local_storage === '/about') {
+        pages[1].style.zIndex = 1;
+        pages[1].style.opacity = 1;
+        navlinks[1].classList.add('active');
+        window.history.replaceState({}, '', '/about');
+        page_idx = { current: 1, prev: 0 };
+      }
+      else if (path_local_storage === '/portfolio') {
+        pages[2].style.zIndex = 1;
+        pages[2].style.opacity = 1;
+        navlinks[2].classList.add('active');
+        window.history.replaceState({}, '', '/portfolio');
+        page_idx = { current: 2, prev: 0 };
+      }
+      else if (path_local_storage === '/contact') {
+        pages[3].style.zIndex = 1;
+        pages[3].style.opacity = 1;
+        navlinks[3].classList.add('active');
+        window.history.replaceState({}, '', '/contact');
+        page_idx = { current: 3, prev: 0 };
+      }
+      else if (path_local_storage === '/3D') {
+        pages[4].style.zIndex = 1;
+        pages[4].style.opacity = 1;
+        navlinks[4].classList.add('active');
+        window.history.replaceState({}, '', '/3D');
+        page_idx = { current: 4, prev: 0 };
       }
 
-    });
-  } // else
-});
+      gsap.fromTo(document.querySelector('body'), {opacity: 0}, {opacity: 1, duration: 1, delay: 0.5})
+
+
+      listenForEvent('local-storage-path-modified', () => {
+        console.log('Event Fired: path changed and local storage "path" set');
+
+        const path_local_storage = JSON.parse(window.localStorage.getItem('path'));
+        if (path_local_storage === '/') {
+          startSmoothScroll();
+        }
+        else {
+          stopSmoothScroll();
+        }
+
+      });
+    } // else
+  });
+
+};
+setupPageTransition();
 
 // ==============================================
 
