7b82dcdbdb1b72a828dc25f0ecde3a22c7d93523
Joshua Holloway <jhollow6@asu.edu>
2022-11-12 19:02:18 -0600

    load animation

--- web-comps/page-0.js
+++ web-comps/page-0.js
@@ -2,12 +2,83 @@ const template = document.createElement('template');
 template.innerHTML = `
   <style>
     * {
-      color: green;
+      margin: 0;
+      padding: 0;
+    }
+    body {
+      background-color: #e0e0e0;
+      overflow: hidden;
+    }
+    span {
+      overflow: hidden;
+      display: block;
+    }
+    .loader-wrap {
+      position: absolute;
+      z-index: 10;
+      height: 100vh;
+      width: 100%;
+      display: flex;
+      overflow: hidden;
+      align-items: center;
+      justify-content: center;
+      background: transparent;
+    }
+    .loader-wrap svg {
+      position: absolute;
+      top: 0;
+      width: 100vw;
+      height: 110vh;
+    }
+    .loader-wrap .loader-wrap-heading h1 {
+      font-size: 120px;
+      z-index: 20;
+      color: #e0e0e0;
+      text-transform: uppercase;
+      font-weight: lighter;
+      font-family: "Ogg";
+    }
+    .container {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      height: 100vh;
+      width: 100%;
+    }
+    .container h1 {
+      font-family: "Ogg";
+      font-weight: lighter;
+      font-size: 100px;
+      text-transform: uppercase;
+    }
+
+    @media (max-width: 767px) {
+      .loader-wrap svg {
+        width: 200vw;
+        margin-left: -50vw;
+      }
+      .loader-wrap .loader-wrap-heading,
+      .container h1 {
+        font-size: 60px;
+      }
     }
   </style>
 
   <main>
-    <h1>Web Comp - Page 0</h1>
+    <div class="loader-wrap">
+      <svg viewBox="0 0 1000 1000" preserveAspectRatio="none">
+        <path id="svg" d="M0,1005S175,995,500,995s500,5,500,5V0H0Z"></path>
+      </svg>
+
+      <!-- <div class="loader-wrap-heading">
+        <span><h1>Loading</h1></span>
+      </div> -->
+    </div>
+    <div class="container">
+      <span>
+        <h1>Josh Holloway</h1>
+      </span>
+    </div>
   </main>
 `;
 
@@ -19,7 +90,47 @@ class WebComp extends HTMLElement {
   }
 
   connectedCallback() {
-    const elem = this.shadowRoot.querySelector('page');
+
+    const svg = this.shadowRoot.getElementById("svg");
+    const tl = gsap.timeline();
+    const curve = "M0 502S175 272 500 272s500 230 500 230V0H0Z";
+    const flat = "M0 2S175 1 500 1s500 1 500 1V0H0Z";
+
+    // tl.from(".loader-wrap-heading h1", {
+    //   delay: 1,
+    //   y: 200,
+    //   skewY: 10,
+    // }).to(".loader-wrap-heading h1", {
+    //   delay: 1.5,
+    //   y: -200,
+    //   skewY: 10,
+    // });
+    tl.to(svg, {
+      duration: 0.8,
+      attr: { d: curve },
+      ease: "power2.easeIn",
+      delay: 1
+    }).to(svg, {
+      duration: 0.8,
+      attr: { d: flat },
+      ease: "power2.easeOut",
+    });
+    tl.to(".loader-wrap", {
+      y: -1500,
+    });
+    tl.to(".loader-wrap", {
+      zIndex: -1,
+      display: "none",
+    });
+    tl.from(
+      ".container h1",
+      {
+        y: 100,
+        opacity: 0,
+      },
+      "-=1.5"
+    );
+
   }
 
   disconnectedCallback() {
