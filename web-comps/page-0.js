522fb171e72a418752b13daacca5df96fe1cdccd
Joshua Holloway <jhollow6@asu.edu>
2022-11-28 19:52:00 -0600

    scroll animation

--- web-comps/page-0.js
+++ web-comps/page-0.js
@@ -29,27 +29,7 @@ template.innerHTML = `
       width: 100vw;
       height: 110vh;
     }
-    .loader-wrap .loader-wrap-heading h1 {
-      font-size: 120px;
-      z-index: 20;
-      color: #e0e0e0;
-      text-transform: uppercase;
-      font-weight: lighter;
-      font-family: "Ogg";
-    }
-    .container {
-      display: flex;
-      align-items: center;
-      justify-content: center;
-      height: 100vh;
-      width: 100%;
-    }
-    .container h1 {
-      font-family: "Ogg";
-      font-weight: lighter;
-      font-size: 100px;
-      text-transform: uppercase;
-    }
+
 
     @media (max-width: 767px) {
       .loader-wrap svg {
@@ -68,18 +48,26 @@ template.innerHTML = `
       <svg viewBox="0 0 1000 1000" preserveAspectRatio="none">
         <path id="svg" d="M0,1005S175,995,500,995s500,5,500,5V0H0Z"></path>
       </svg>
-
-      <!-- <div class="loader-wrap-heading">
-        <span><h1>Loading</h1></span>
-      </div> -->
     </div>
     <div class="container">
       
       <wc-hero></wc-hero>
 
+      <style>
+        section {
+          height: 100vh;
+        }
+      </style>
+      <section id="about">
+        <div class="animate-box" style="height: 100px; width: 100px; background: red;"></div>
+        <h3 class="animate-text">Text goes here...</h3>
+      </section>
+
     </div>
   </main>
 `;
+
+// ==============================================
 
 class WebComp extends HTMLElement {
   constructor() {
@@ -87,54 +75,74 @@ class WebComp extends HTMLElement {
     this.attachShadow({ mode: 'open'})
     this.shadowRoot.appendChild(template.content.cloneNode(true));
   }
-
+  
+  // --------------------------------------------
+  
   connectedCallback() {
-
-    const svg = this.shadowRoot.getElementById("svg");
-    const tl = gsap.timeline();
-    const curve = "M0 502S175 272 500 272s500 230 500 230V0H0Z";
-    const flat = "M0 2S175 1 500 1s500 1 500 1V0H0Z";
-
-    // tl.from(".loader-wrap-heading h1", {
-    //   delay: 1,
-    //   y: 200,
-    //   skewY: 10,
-    // }).to(".loader-wrap-heading h1", {
-    //   delay: 1.5,
-    //   y: -200,
-    //   skewY: 10,
-    // });
-    tl.to(svg, {
-      duration: 0.8,
-      attr: { d: curve },
-      ease: "power2.easeIn",
-      delay: 1
-    }).to(svg, {
-      duration: 0.8,
-      attr: { d: flat },
-      ease: "power2.easeOut",
-    });
-    tl.to(".loader-wrap", {
-      y: -1500,
-    });
-    tl.to(".loader-wrap", {
-      zIndex: -1,
-      display: "none",
-    });
-    tl.from(
-      ".container h1",
-      {
-        y: 100,
+    
+    const qs = x => this.shadowRoot.querySelector(x);
+    const qsAll = x => this.shadowRoot.querySelectorAll(x);
+    
+    // - - - - - - - - - - - - - - - - - - - - - 
+    
+    const curtainAnim = () => {
+      const svg = qs("#svg");
+      const tl = gsap.timeline();
+      const curve = "M0 502S175 272 500 272s500 230 500 230V0H0Z";
+      const flat = "M0 2S175 1 500 1s500 1 500 1V0H0Z";
+      tl.to(svg, {
+        duration: 0.8,
+        attr: { d: curve },
+        ease: "power2.easeIn",
+        delay: 1
+      }).to(svg, {
+        duration: 0.8,
+        attr: { d: flat },
+        ease: "power2.easeOut",
+      });
+    };
+    curtainAnim();
+    
+    // - - - - - - - - - - - - - - - - - - - - - 
+    
+    const sectionAnimation = () => {
+      
+      const tl = gsap.timeline({
+        scrollTrigger: {
+          trigger: qs('#about'),
+          start: 'center bottom',
+          // start: 'top bottom',
+          // toggleActions: "play pause resume reset"
+          toggleActions: "play pause resume reverse"
+        }
+      });
+      
+      tl.from(qs('.animate-box'), {
+        x: 200,
         opacity: 0,
-      },
-      "-=1.5"
-    );
+        duration: 1.1
+      });
+      
+      tl.from(qs('.animate-text'), {
+        y: 200,
+        opacity: 0,
+        duration: 1
+      }, '-=1');
 
+    };
+    sectionAnimation();
+    
+    // - - - - - - - - - - - - - - - - - - - - - 
+    
   }
 
+  // --------------------------------------------
+  
   disconnectedCallback() {
     const elem = this.shadowRoot.querySelector('page')
   }
 }
+
+// ==============================================
 
 window.customElements.define('page-0', WebComp);
